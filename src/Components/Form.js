9af762d57c53de1cd8c6c229cbecbdb6c008aca1
josscmorera <63267272+josscmorera@users.noreply.github.com>
2023-06-12 12:03:56 -0400

    react and Componenets install

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function Form() {
-  return (
-    <div>Form</div>
-  )
-}
+const Form = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
 
-export default Form
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+        </div>
+    );
+};
+
+export default Form;
